7dadd1134e638c3c5807aa688c611064f1d0a35e
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-03-03 09:31:39 +0200

    updated

--- EdgeSnap/EdgeSnap/urls.py
+++ EdgeSnap/EdgeSnap/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls')),
-    path('api/',include('api.urls')),
+    path('api/',include('core.api.urls')),
 ]
