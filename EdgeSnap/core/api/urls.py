c6b4067db3feac780b8165fddd3216ce256e6fed
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-03-03 11:43:50 +0200

    updated

--- EdgeSnap/core/api/urls.py
+++ EdgeSnap/core/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path('get-routes/', views.get_routes),
     path('get-grayscale/', views.get_grayscale),
+    path('token/', views.myTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- EdgeSnap/core/api/views.py
+++ EdgeSnap/core/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import MyTokenObtainPairSerializer
 from rest_framework import status
 import cv2 as cv
 
 
-
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def get_routes(request):
